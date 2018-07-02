28206fe481823cdc2128db55fb7af5154ec39dc7
clx <1360463840@qq.com>
2018-07-02 21:11:14 +0800

    第一次部署

---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<%@ page pageEncoding="utf-8" %>
+<html>
+<body>
+<h2>Hello World!  陈龙祥afdsadsdfdf</h2>
+</body>
+</html>
---
+++ src/test/java/com/clx/test/test1.java
@@ -0,0 +1,13 @@
+package com.clx.test;
+
+import org.junit.Test;
+
+public class test1 {
+
+
+    @Test
+    public void testMaven(){
+
+    }
+
+}
